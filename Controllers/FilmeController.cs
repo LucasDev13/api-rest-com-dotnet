d2201d3110e2bdd536e4a5fd43ff865244b49006
Lucas Pontes <lucasdev@pop-os.localdomain>
2021-10-18 15:19:59 -0300

    Definindo o retorno das rotas.

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -12,25 +12,27 @@ namespace api_v1_dotNetFlix.Controllers
          private static int id = 1;
 
          [HttpPost]
-         public void AdicionaFilme([FromBody] Filme filme){
+         public IActionResult AdicionaFilme([FromBody] Filme filme){
             filme.Id = id++; 
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id}, filme);
          }
 
          [HttpGet]
-         public IEnumerable<Filme> RecuperarFilmes(){
-             return filmes;
+         public IActionResult RecuperarFilmes(){
+             return Ok(filmes);
          }
 
          [HttpGet("{id}")]
-         public Filme RecuperaFilmesPorId(int id){
+         public IActionResult RecuperaFilmesPorId(int id){
              foreach(Filme filme in filmes){
                  if(filme.Id == id){
-                    return filme;
+                    //retorna o status 200-sucesso
+                    return Ok(filme);
                  }
              }
-             return null;
+             //retorna 404-notfound
+             return NotFound();
              
          }
 
