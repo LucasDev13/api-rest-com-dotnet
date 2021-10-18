8a167c1402bced97651309636d07d27c3712b82a
Lucas Pontes <lucasdev@pop-os.localdomain>
2021-10-18 17:05:36 -0300

    Escrevendo e lendo dados do banco de dados.

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -2,35 +2,41 @@ using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using api_v1_dotNetFlix.Models;
+using api_v1_dotNetFlix.Data;
 
 namespace api_v1_dotNetFlix.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class FilmeController : ControllerBase{
-         private static List<Filme> filmes = new List<Filme>();
-         private static int id = 1;
+        
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context){
+            _context = context;
+        }
+
 
          [HttpPost]
          public IActionResult AdicionaFilme([FromBody] Filme filme){
-            filme.Id = id++; 
-            filmes.Add(filme);
+             //adiciona um filme ao contexto.
+            _context.Filmes.Add(filme);
+            //salva os dados no banco.
+            _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id}, filme);
          }
 
          [HttpGet]
          public IActionResult RecuperarFilmes(){
-             return Ok(filmes);
+             return Ok(_context.Filmes);
          }
 
          [HttpGet("{id}")]
          public IActionResult RecuperaFilmesPorId(int id){
-             foreach(Filme filme in filmes){
-                 if(filme.Id == id){
-                    //retorna o status 200-sucesso
-                    return Ok(filme);
-                 }
-             }
+            var filme = _context.Filmes.Find(id);
+            if(filme != null){
+                return Ok(filme);      
+            }             
              //retorna 404-notfound
              return NotFound();
              
