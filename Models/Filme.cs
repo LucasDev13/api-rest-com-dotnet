f7d528403ed44dfd1394ff21e75d2b81acac9a05
Lucas Pontes <lucasdev@pop-os.localdomain>
2021-10-18 11:16:40 -0300

    Validando parâmetros recebidos da requisição.

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -5,13 +5,13 @@ namespace api_v1_dotNetFlix.Models
 {
     public class Filme
     {
-        
+        [Required(ErrorMessage ="O campo titulo é obrigatório.")]
         public string Titulo { get; set; }
-       
+        [Required(ErrorMessage ="O campo titulo é obrigatório.")]
         public string Diretor { get; set; }
-        
+        [StringLength(30, ErrorMessage = "Máximo de caracteres - 30")]
         public string Genero { get; set; }
-        
+        [Range(1, 600, ErrorMessage = "A duaração dever ser entre 1 e 600 min.")]
         public int Duracao { get; set; }
     }
 }
